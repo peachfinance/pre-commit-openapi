72cb177c58c78903eb7557a57a9998fff5e55c1b
Chris Wilcox <ckwilcox@gmail.com>
2023-09-27 16:17:09 -0700

    pin jsonschema version to prevent an error

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['openapi-spec-validator==0.4.0'],
+    install_requires=[
+        'jsonschema==3.2.0', 
+        'openapi-spec-validator==0.4.0'
+    ],
     scripts=['bin/check-many-openapi'],
 )
